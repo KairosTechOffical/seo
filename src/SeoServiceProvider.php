b6733c6d37cc014a4da57cf037b8f7e0d73a4e1a
KairosTechOffical <kairostheopportunemoment@gmail.com>
2025-06-13 13:15:17 +0500

    Update SeoServiceProvider.php
    updated provider

--- src/SeoServiceProvider.php
+++ src/SeoServiceProvider.php
@@ -8,12 +8,12 @@ class SeoServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'seo');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'seo');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         $this->publishes([
-            __DIR__.'/resources/views' => resource_path('views/vendor/seo'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/seo'),
         ], 'seo-views');
     }
 
